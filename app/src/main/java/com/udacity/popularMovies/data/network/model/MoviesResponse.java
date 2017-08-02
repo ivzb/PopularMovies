8ddfc9a8af379fe5a975a7b9dddfd5690df9d6a1
izahariev <izahariev96@gmail.com>
2017-08-02 18:15:00 +0300

    :construction: details layout

--- app/src/main/java/com/udacity/popularMovies/data/network/model/MoviesResponse.java
+++ app/src/main/java/com/udacity/popularMovies/data/network/model/MoviesResponse.java
@@ -100,6 +100,9 @@ public class MoviesResponse {
         @SerializedName("title")
         private String title;
 
+        @SerializedName("overview")
+        private String overview;
+
         @SerializedName("popularity")
         private double popularity;
 
@@ -162,6 +165,14 @@ public class MoviesResponse {
 
         public void setTitle(String title) {
             this.title = title;
+        }
+
+        public String getOverview() {
+            return overview;
+        }
+
+        public void setOverview(String overview) {
+            this.overview = overview;
         }
 
         public double getPopularity() {
@@ -242,6 +253,8 @@ public class MoviesResponse {
             if (Double.compare(movie.popularity, popularity) != 0) return false;
             if (adult != movie.adult) return false;
             if (title != null ? !title.equals(movie.title) : movie.title != null) return false;
+            if (overview != null ? !overview.equals(movie.overview) : movie.overview != null)
+                return false;
             if (posterPath != null ? !posterPath.equals(movie.posterPath) : movie.posterPath != null)
                 return false;
             if (originalLanguage != null ? !originalLanguage.equals(movie.originalLanguage) : movie.originalLanguage != null)
@@ -265,6 +278,7 @@ public class MoviesResponse {
             temp = Double.doubleToLongBits(voteAverage);
             result = 31 * result + (int) (temp ^ (temp >>> 32));
             result = 31 * result + (title != null ? title.hashCode() : 0);
+            result = 31 * result + (overview != null ? overview.hashCode() : 0);
             temp = Double.doubleToLongBits(popularity);
             result = 31 * result + (int) (temp ^ (temp >>> 32));
             result = 31 * result + (posterPath != null ? posterPath.hashCode() : 0);
